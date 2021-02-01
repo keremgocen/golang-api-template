b9dd4092d5c4388d3e9001dd8109b0cf488aa15d
keremgocen <keremgocen@gmail.com>
2021-02-01 03:27:22 +0300

    implement cr-d endpoints, swagger init

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/keremgocen/golang-api-template
 go 1.15
 
 require (
+	github.com/go-openapi/loads v0.20.1 // indirect
+	github.com/go-openapi/spec v0.20.2 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/go-swagger/go-swagger v0.26.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/spf13/afero v1.5.1 // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/tools v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
+// Package classification Account API.
+//
+// this is to show how to write RESTful APIs in golang.
+// that is to provide a detailed overview of the language specs
+//
+// Terms Of Service:
+//
+//     Schemes: http, https
+//     Host: localhost:8080
+//     Version: 1.0.0
+//     Contact: Kerem Gocen<my@email.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - api_key:
+//
+//     SecurityDefinitions:
+//     api_key:
+//          type: apiKey
+//          name: KEY
+//          in: header
+//
+// swagger:meta
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,12 +41,89 @@ import (
 )
 
 type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	UserRef     string `json:"userRef"`
+	Name        string `json:"name"`
+	DOB         string `json:"dob"`
+	PhoneNumber string `json:"phoneNumber"`
+	Address     string `json:"address"`
 }
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
+// let's declare a global Persons array
+// that we can then populate in our main function
+// to simulate a database
+var Persons []Person
+
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
+}
+
+func returnAllPersons(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllPersons")
+	json.NewEncoder(w).Encode(Persons)
+}
+
+func returnSinglePerson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["userRef"]
+
+	// Loop over all of our Persons
+	// if the Person.Id equals the key we pass in
+	// return the Person encoded as JSON
+	for _, person := range Persons {
+		if person.UserRef == key {
+			json.NewEncoder(w).Encode(person)
+		}
+	}
+}
+
+func createNewPerson(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Person struct
+	// append this to our Persons array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var person Person
+	json.Unmarshal(reqBody, &person)
+	// update our global Persons array to include
+	// our new Person
+	Persons = append(Persons, person)
+
+	json.NewEncoder(w).Encode(person)
+}
+
+func deletePerson(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the person we
+	// wish to delete
+	id := vars["userRef"]
+
+	// we then need to loop through all our persons
+	for index, person := range Persons {
+		// if our id path parameter matches one of our
+		// persons
+		if person.UserRef == id {
+			// updates our Persons array to remove the
+			// person
+			Persons = append(Persons[:index], Persons[index+1:]...)
+		}
+	}
+
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllPersons)
+	myRouter.HandleFunc("/person/{id}", returnSinglePerson)
+	myRouter.HandleFunc("/person", createNewPerson).Methods("POST")
+	myRouter.HandleFunc("/person/{id}", deletePerson).Methods("DELETE")
+
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
@@ -27,7 +133,7 @@ func main() {
 		DB:       0,
 	})
 
-	json, err := json.Marshal(Person{Name: "Kerem", Age: 35})
+	json, err := json.Marshal(Person{UserRef: "KFG-734", Name: "Kerem", DOB: "18.06.1985", PhoneNumber: "+901111111111", Address: "Woodbine Close TW2 something"})
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -42,7 +148,10 @@ func main() {
 	}
 	fmt.Println(val)
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	Persons = []Person{
+		Person{UserRef: "KFG-734", Name: "Kerem", DOB: "18.06.1985", PhoneNumber: "+441111111111", Address: "Woodbine Close TW2"},
+		Person{UserRef: "XRT-251", Name: "John", DOB: "11.11.1111", PhoneNumber: "+442222222222", Address: "Somewhere in London"},
+	}
+
+	handleRequests()
 }
